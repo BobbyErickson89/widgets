3161dae32da7f943f7cfed097e6b4192f5e25663
Bobby Erickson <ericksonr07@gmail.com>
2015-01-27 19:24:36 -0500

    first commit

---
+++ dropdownmenu.js
@@ -0,0 +1,28 @@
+var adminButton = document.querySelector(".admin-settings-button");
+
+
+function toggleClass(element, className) {
+  var currentClasses = element.className;
+
+  if (currentClasses.indexOf(className) >= 0) {
+    element.className = currentClasses.replace(className, '').trim();
+  } else {
+    element.className += ' ' + className;
+  };
+};
+
+
+
+
+adminButton.onclick = toggleClass(, ".admin-settings-list");
+
+
+
+
+//
+// stuff from online
+// function toggleContent() {
+//   var toggleID = document.querySelector("admin-settings-list");
+//   toggleID.style.display = "block" ? toggleID.style.display = toggleID.style.display =
+//   "block";
+// };
